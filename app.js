887029af4daf1c87ff59b1ac323b5474534c0ac2
fkaany <136654421+fkaany@users.noreply.github.com>
2025-11-06 22:36:34 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-11-06T19:36:29.197Z
-Commit: Implement user profile customization
+Timestamp: 2025-11-06T19:36:33.785Z
+Commit: Fix CSS styling bug on mobile devices
